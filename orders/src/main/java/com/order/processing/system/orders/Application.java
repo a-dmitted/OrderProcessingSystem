291d43dcda5f76b419f0ccc44423dc4d3cd6b9f6
admitted <dmitriev.alexander@gmail.com>
2024-03-07 09:18:45 +0100

    CORS: add origin patterns

--- orders/src/main/java/com/order/processing/system/orders/Application.java
+++ orders/src/main/java/com/order/processing/system/orders/Application.java
@@ -1,9 +1,12 @@
 package com.order.processing.system.orders;
 
+import com.order.processing.system.common.WebConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(WebConfig.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
